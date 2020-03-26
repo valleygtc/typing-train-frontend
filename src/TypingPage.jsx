af27fb899e84ef292c5341f70a84113adaa36d26
gutianci <gutianci@pwrd.com>
2020-03-26 19:22:51 +0800

    feat: 打字速度报告

--- src/TypingPage.jsx
+++ src/TypingPage.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { message } from 'antd';
+import React, { useState, useEffect, useRef } from 'react';
+import { message, Modal } from 'antd';
 
 import { fetchArticle, zip } from './utils';
 
@@ -13,6 +13,7 @@ export default function TypingPage({
     content: '',
   });
   const [ input, setInput ] = useState('');
+  const timer = useRef({});
 
   const params = new URLSearchParams(location.search);
   const id = params.get('id');
@@ -66,33 +67,85 @@ export default function TypingPage({
     }
   });
 
-  const inputItems = Array.from(zip(article.content, input)).map(([c1, c2], index) => (
-    c1 === c2
-    ? <span key={index} style={{
-        background: '#e7fbd3',
-        color: '#0e630e',
-      }}>{c1}</span>
-    : <span key={index} style={{
-        background: 'pink',
-        color: 'darkred',
-      }}>{c1}</span>)
-  );
+  // timer
+  useEffect(() => {
+    if (input.length >= 1 && !timer.current.start) {
+      // start timer
+      timer.current = {
+        start: Date.now(),
+      };
+    }
+
+    if (timer.current.start && input.length === article.content.length) {
+      // end
+      const duration = Date.now() - timer.current.start; // ms
+      const seconds = Math.round(duration / 1000); // s
+      const WPM = Math.round((input.length / 5) / (seconds / 60));
+      console.log({ timer, timeCount: duration, seconds });
+      Modal.info({
+        icon: null,
+        title: `打字速度报告`,
+        content: (
+          <div>
+            <div>{`总花费时间：${seconds} 秒。`}</div>
+            <div>{`WPM：${WPM}`}</div>
+          </div>
+        ),
+        width: '800px',
+        onOk() {
+          history.goBack();
+        },
+      });
+    }
+  })
+
+  const inputItems = Array.from(zip(article.content, input)).map(([c1, c2], index) => {
+    if (c1 === c2) {
+      return (
+        <span key={index} style={{
+          background: '#e7fbd3',
+          color: '#0e630e',
+        }}>{c1}</span>
+      )
+    } else {
+      return (
+        <span key={index} style={{
+          background: 'pink',
+          color: 'darkred',
+        }}>{c1}</span>
+      )
+    }
+  });
+
+  const restItems = Array.from(article.content.slice(input.length)).map((c, index) => {
+    const key = input.length + index;
+    if (index === 0) {
+      return (
+        <span key={key} style={{
+          borderBottom: '3px solid #0a6bf9',
+        }}>
+          {c}
+        </span>
+      )
+    } else {
+      return (
+        <span key={key}>{c}</span>
+      );
+    }
+  });
 
   return (
     <div
       style={{
         fontSize: '35px',
+        fontFamily: 'Courier, monospace',
+        whiteSpace: 'pre',
+        display: 'flex',
+        flexWrap: 'wrap',
       }}
     >
-      <pre>
-        {inputItems}
-        <span style={{
-          borderBottom: '3px solid #0a6bf9',
-        }}>
-          {article.content[input.length]}
-        </span>
-        {article.content.slice(input.length + 1)}
-      </pre>
+      {inputItems}
+      {restItems}
     </div>
   )
 }
